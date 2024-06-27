60c4336e6c6c330e75fbed961ab75470bc3120ed
Anvar571 <abdurashidovanvar52@gmail.com>
2024-06-28 02:27:51 +0500

    feat: user register and login

--- stack-overflow/StackOverflowDemo.ts
+++ stack-overflow/StackOverflowDemo.ts
@@ -1,6 +1,27 @@
+import { StackOverflowSystem } from "./StackOverflowSystem";
+import { User } from "./Users";
 
 export class Main{
-    public static run() {}
+    public static run() {
+        const stackOverflow = StackOverflowSystem.getInstanse();
+
+        const user1 = new User({
+            email: "anvar@gmail.com",
+            username: "anvar",
+            reputation: "1",
+            password: {
+                password: "12345678"
+            }
+        });
+
+        stackOverflow.register(user1);
+        const user = stackOverflow.login(user1.getUsername, user1.getPassword.getPassword);
+        if (!user){
+            console.log("User not found, Please Register now");
+        } else {
+            console.log("Loggin successfully");
+        }
+    }
 }
 
 Main.run();
--- stack-overflow/StackOverflowSystem.ts
+++ stack-overflow/StackOverflowSystem.ts
@@ -1,8 +1,17 @@
+import { Answer } from "./Answer";
+import { Question } from "./Question";
+import { Tag } from "./Tag";
+import { User } from "./Users";
 
 export class StackOverflowSystem{
+    private users: User[] = [];
+    private questions: Question[] = [];
+    private answers: Answer[] = [];
+    private tags: Tag[] = [];
+    private comments: Comment[] = [];
     private static instanse: StackOverflowSystem | null;
 
-    private constructor(){}
+    private constructor() {}
 
     public static getInstanse() {
         if(this.instanse){
@@ -11,5 +20,46 @@ export class StackOverflowSystem{
         return new StackOverflowSystem();
     }
 
+    public register(newUser: User) {
+        const user = this.users.find((val, ind) => val.getUsername === newUser.getUsername);
+
+        if(user){
+            throw new Error("This user already registered")
+        }
+
+        this.users.push(newUser);
+    }
+
+    public login(username: string, password: string): User | boolean {
+        const user = this.users.find((val, ind) => val.getUsername === username);
+        if(user){
+            if (user.getPassword.getPassword !== password) {
+                throw new Error("This is user username or password incorrect")
+            }
+            return user
+        }
+        
+        return false
+    }
+
+    public addQuestion(newQuestion: Question) {
+        const question = this.questions.find((val) => val.getTitle === newQuestion.getTitle);
+
+        if (question) {
+            throw new Error("This question already exists")
+        }
+
+        this.questions.push(newQuestion);
+    }
+
+    public addAnswer(questionId: number, answer: Answer) {
+        const question = this.questions.find((val) => val.getId === questionId);
+
+        if (!question) {
+            throw new Error("this question is not found")
+        }
+
+        this.questions.map(item => item.getId === questionId ? {answers: answer}: item)
+    }
     
 }
