ccbfa5066995cfea4813014250a160f70d3e0d31
Anvar571 <abdurashidovanvar52@gmail.com>
2024-07-22 23:20:41 +0500

    in progress RentalSystem

--- car rental system/RentalSystem.ts
+++ car rental system/RentalSystem.ts
@@ -1,4 +1,11 @@
+import { Car } from "./Car";
+import { Customer } from "./Customer";
+import {Reservation} from "./Reservation";
+
 export class RentalSystem {
+    private cars: Car[] = [];
+    private reservations: Reservation[] = [];
+
     private static instance: RentalSystem;
     private constructor() {}
 
@@ -6,4 +13,18 @@ export class RentalSystem {
         if (this.instance) return this.instance;
         return new RentalSystem();
     }
+
+    addCar(car: Car) {}
+
+    removeCar(car: Car) {}
+
+    searchCars(value: string) {}
+
+    makeReservation(customer: Customer, car: Car, start_date: DataTransfer, end_date: Date) {
+
+    }
+
+    processPayment(reservation: Reservation){}
+
+    cancelReservation() {}
 }
