7ba9edd6cdc94065e6cb16841da551a626f9221a
Anvar571 <abdurashidovanvar52@gmail.com>
2024-08-02 23:58:35 +0500

    changed transaction class

--- atm system/Transaction.ts
+++ atm system/Transaction.ts
@@ -1,13 +1,36 @@
+import { Account } from "./Account";
+
 export abstract class Transaction {
-    payment: (x?: number) => number;
+    protected transactionId: number;
+    protected account: Account;
+    protected amount: number;
+
+    constructor(data: {transactionId: number, account: Account, amount: number}) {
+        this.transactionId = data.transactionId;
+        this.account = data.account;
+        this.amount = data.amount;
+    }
+
+    public abstract execute: () => any;
 }
 
-// bank hisobidan pull olish
-export class WithdrawalTransaction implements Transaction {
-    payment: (x?: number | undefined) => number;
+export class WithdrawalTransaction extends Transaction {
+    constructor(data: {transactionId: number, account: Account, amount: number}) {
+        super(data);
+    }
+
+    execute()  {
+        console.log("ads");
+    }
+   
 }
 
 // pull qo'yish
-export class DepositTransaction implements Transaction {
-    payment: (x?: number | undefined) => number;
+export class DepositTransaction extends Transaction {
+    constructor(data: {transactionId: number, account: Account, amount: number}) {
+        super(data);
+    }
+    execute()  {
+        
+    }
 }
